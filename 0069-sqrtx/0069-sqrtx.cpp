01af11ae6a29ac1b21f12140392d65b7c7ca3cfe
LokeshRaju <loki65628@gmail.com>
2026-02-13 12:23:19 +0530

    Time: 0 ms (100%), Space: 8.5 MB (85.21%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int mySqrt(int num) {
+         long long l = 1,h = num;
+       
+        while(l <= h)
+        {
+            long long mid = l + (h - l) / 2;
+            if(mid * mid == num)
+            {
+                return mid;
+            }
+            else if(mid * mid < num)
+            {
+                l = mid + 1;
+            }
+            else
+            {
+                h = mid - 1;
+            }
+        }
+
+        return l - 1;
+    }
+};
