66d408d5895f3aa4a707ddaf0ae7828bc4c36cb5
LokeshRaju <loki65628@gmail.com>
2026-02-15 17:51:59 +0530

    Time: 55 ms (71.41%), Space: 10.5 MB (52.91%) - LeetHub

--- 3844-longest-almost-palindromic-substring/3844-longest-almost-palindromic-substring.cpp
+++ 3844-longest-almost-palindromic-substring/3844-longest-almost-palindromic-substring.cpp
@@ -1,101 +1,136 @@
 class Solution {
 public:
     int almostPalindromic(string s) {
-        int maxi = 2;
-
+        int maxi = 2;//because if there is "ab" we can dlt one char and we will get 2 len
         int n = s.size();
-
-        for (int i = 0; i < n; i++) {
-
-            int j = i - 1;
-            int k = i + 1;
-            bool flag = true;
-            while (j >= 0 && k < n) {
-                if (s[j] != s[k]) {
-                    if (flag) {
-                        flag = false;
-                        j--;
-                    } else {
+        for(int i = 0;i < n;i++)
+        {
+            //for odd length palindrome
+            //also checking I can include one char in my subarray after deleting an character by left side
+            int l = i - 1;
+            int r = i + 1;
+            bool used = true;
+            while(l >= 0 && r < n)
+            {
+                if(s[l] != s[r])
+                {
+                    if(used)
+                    {
+                        used = false;
+                        l--;
+                    }
+                    else
+                    {
                         break;
                     }
-                } else {
-                    maxi = max(maxi, k - j + 1);
-                    if ((k < n - 1 || j > 0) && flag) {
-                        maxi = max(maxi, k - j + 2);
+                }
+                else
+                {
+                    maxi = max(maxi,r - l + 1);//length of the substring
+                    if((r < n - 1 || l > 0) && used) //if I not removed one char also till now
+                    {
+                        maxi = max(maxi,r - l + 2);//I can take one mismatch character into substring 
                     }
-                    j--;
-                    k++;
+                    l--;
+                    r++;
                 }
             }
 
-            j = i - 1;
-            k = i + 1;
-            flag = true;
-
-            while (j >= 0 && k < n) {
-                if (s[j] != s[k]) {
-                    if (flag) {
-                        flag = false;
-                        k++;
-                    } else {
+            //for odd length palindrome
+            //also checking I can include one char in my subarray after deleting an character by right side
+            l = i - 1;
+            r = i + 1;
+            used = true;
+            while(l >= 0 && r < n)
+            {
+                if(s[l] != s[r])
+                {
+                    if(used)
+                    {
+                        used = false;
+                        r++;
+                    }
+                    else
+                    {
                         break;
                     }
-                } else {
-                    maxi = max(maxi, k - j + 1);
-                    if ((k < n - 1 || j > 0) && flag) {
-                        maxi = max(maxi, k - j + 2);
+                }
+                else
+                {
+                    maxi = max(maxi,r - l + 1);//length of the substring
+                    if((r < n - 1 || l > 0) && used) //if I mot removed one char also till now
+                    {
+                        maxi = max(maxi,r - l + 2);//I can take one mismatch character into substring 
                     }
-                    j--;
-                    k++;
+                    l--;
+                    r++;
                 }
             }
 
-            j = i;
-            k = i + 1;
-            flag = true;
-
-            while (j >= 0 && k < n) {
-                if (s[j] != s[k]) {
-                    if (flag) {
-                        flag = false;
-                        j--;
-                    } else {
+            //for EVEN LENGTH palindrome
+            //also checking I can include one char in my subarray after deleting an character by left side
+             l = i;
+             r = i + 1;
+             used = true;
+            while(l >= 0 && r < n)
+            {
+                if(s[l] != s[r])
+                {
+                    if(used)
+                    {
+                        used = false;
+                        l--;
+                    }
+                    else
+                    {
                         break;
                     }
-                } else {
-                    maxi = max(maxi, k - j + 1);
-                    if ((k < n - 1 || j > 0) && flag) {
-                        maxi = max(maxi, k - j + 2);
+                }
+                else
+                {
+                    maxi = max(maxi,r - l + 1);//length of the substring
+                    if((r < n - 1 || l > 0) && used) //if I not removed one char also till now
+                    {
+                        maxi = max(maxi,r - l + 2);//I can take one mismatch character into substring 
                     }
-                    j--;
-                    k++;
+                    l--;
+                    r++;
                 }
             }
 
-            j = i;
-            k = i + 1;
-            flag = true;
-
-            while (j >= 0 && k < n) {
-                if (s[j] != s[k]) {
-                    if (flag) {
-                        flag = false;
-                        k++;
-                    } else {
+            //for EVEN length palindrome
+            //also checking I can include one char in my subarray after deleting an character by right side
+             l = i;
+             r = i + 1;
+             used = true;
+            while(l >= 0 && r < n)
+            {
+                if(s[l] != s[r])
+                {
+                    if(used)
+                    {
+                        used = false;
+                        r++;
+                    }
+                    else
+                    {
                         break;
                     }
-                } else {
-                    maxi = max(maxi, k - j + 1);
-                    if ((k < n - 1 || j > 0) && flag) {
-                        maxi = max(maxi, k - j + 2);
+                }
+                else
+                {
+                    maxi = max(maxi,r - l + 1);//length of the substring
+                    if((r < n - 1 || l > 0) && used) //if I mot removed one char also till now
+                    {
+                        maxi = max(maxi,r - l + 2);//I can take one mismatch character into substring 
                     }
-                    j--;
-                    k++;
+                    l--;
+                    r++;
                 }
             }
 
-          
         }
+
         return maxi;
     }
 };
