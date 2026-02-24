2e700e1e154646a8b951d35bc41948e5c59ffc48
LokeshRaju <loki65628@gmail.com>
2026-02-24 23:00:13 +0530

    Time: 0 ms (100%), Space: 8.6 MB (93.27%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -1,23 +1,50 @@
 class Solution {
 public:
-    int longestSubstring(string s, int k) 
-    {
-    int len = 0;
-    vector<int>freq(128,0);
-    for(int i = 0;i <s.size();i++)
-    {
-        freq[s[i]]++;
-    }
-    for(int i = 0;i < s.size();i++)
-    {
-        if(freq[s[i]] < k)
-        {
-            int left = longestSubstring(s.substr(0,i),k);
-            int right = longestSubstring(s.substr(i + 1),k);
-            return max(left,right);
-        }
-    }
+    int longestSubstring(string s, int k) {
 
-    return s.size();
+        int n = s.size();
+        int maxLen = 0;
+
+        for (int targetUnique = 1; targetUnique <= 26; targetUnique++) {
+
+            int freq[26] = {0};
+            int si = 0, ei = 0;
+            int unique = 0;
+            int countAtLeastK = 0;
+
+            while (ei < n) {
+
+                // expand window
+                if (freq[s[ei] - 'a'] == 0)
+                    unique++;
+
+                freq[s[ei] - 'a']++;
+
+                if (freq[s[ei] - 'a'] == k)
+                    countAtLeastK++;
+
+                ei++;
+
+                // shrink window
+                while (unique > targetUnique) {
+
+                    if (freq[s[si] - 'a'] == k)
+                        countAtLeastK--;
+
+                    freq[s[si] - 'a']--;
+
+                    if (freq[s[si] - 'a'] == 0)
+                        unique--;
+
+                    si++;
+                }
+
+                // check validity
+                if (unique == targetUnique && unique == countAtLeastK)
+                    maxLen = max(maxLen, ei - si);
+            }
+        }
+
+        return maxLen;
     }
 };
