adf7fcc3f11cd140d1449b004afd266fd93827cb
LokeshRaju <loki65628@gmail.com>
2026-02-25 09:30:48 +0530

    Time: 0 ms (100%), Space: 8.5 MB (97.51%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -1,50 +1,45 @@
 class Solution {
 public:
     int longestSubstring(string s, int k) {
-
         int n = s.size();
-        int maxLen = 0;
-
-        for (int targetUnique = 1; targetUnique <= 26; targetUnique++) {
-
+        int len = 0;
+        for(int targetUnique = 1;targetUnique <= 26;targetUnique++)
+        {
             int freq[26] = {0};
-            int si = 0, ei = 0;
             int unique = 0;
-            int countAtLeastK = 0;
+            int countAtleastk = 0;
+            int ei = 0,si = 0;
 
-            while (ei < n) {
+            while(ei < n)
+            {
+                if(freq[s[ei] - 'a'] == 0) unique++;
 
-                // expand window
-                if (freq[s[ei] - 'a'] == 0)
-                    unique++;
+                freq[s[ei]- 'a']++;
 
-                freq[s[ei] - 'a']++;
-
-                if (freq[s[ei] - 'a'] == k)
-                    countAtLeastK++;
-
+                if(freq[s[ei] - 'a'] == k) countAtleastk++;
                 ei++;
 
-                // shrink window
-                while (unique > targetUnique) {
-
-                    if (freq[s[si] - 'a'] == k)
-                        countAtLeastK--;
+                //shrink window when we get unique characters more than targetUnique
+                while(unique > targetUnique)
+                {
+                    if(freq[s[si] - 'a'] == k) countAtleastk--;
 
                     freq[s[si] - 'a']--;
 
-                    if (freq[s[si] - 'a'] == 0)
-                        unique--;
+                    if(freq[s[si] - 'a'] == 0) unique--;//removing the character in window
 
                     si++;
                 }
 
-                // check validity
-                if (unique == targetUnique && unique == countAtLeastK)
-                    maxLen = max(maxLen, ei - si);
+                if(unique == targetUnique && unique == countAtleastk
+                )
+                {
+                    len = max(len,ei - si);
+                }
             }
+
         }
 
-        return maxLen;
+        return len;
     }
 };
