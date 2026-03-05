a78c17d1e7e1893a69ecb2c9cf3fa83082c5e539
LokeshRaju <loki65628@gmail.com>
2026-03-05 22:49:41 +0530

    Time: 0 ms (100%), Space: 9.4 MB (51.31%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -8,9 +8,10 @@ public:
         for(int i = 0;i < n;i++)
         {
             if(s[i] - '0' == j) count++;//we got a match
-            j ^= 1;
+            j ^= 1;//toggle the bit and check if it matches it is in alternative sequence
         }
 
+        //count will give gives you how many are alteratively moving and n - count gives the mismatch elements
         ans = min(count,n - count);
         return ans;
     }
