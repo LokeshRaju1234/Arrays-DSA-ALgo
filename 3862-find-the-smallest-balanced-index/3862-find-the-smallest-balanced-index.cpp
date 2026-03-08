9cb9e8cd95875b31b396da0c18e2aa3e4874329f
LokeshRaju <loki65628@gmail.com>
2026-03-08 12:14:16 +0530

    Time: 26 ms (20.94%), Space: 225.2 MB (20.5%) - LeetHub

---
+++ 3862-find-the-smallest-balanced-index/3862-find-the-smallest-balanced-index.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int smallestBalancedIndex(vector<int>& nums) {
+        
+        int n = nums.size();
+        //left prefix sum
+        vector<long long>leftSum(n);
+
+        leftSum[0] = 0;//if there is no left element
+        for(int i = 1;i < n;i++)
+        {
+            leftSum[i] = leftSum[i - 1] + (long long)nums[i - 1];
+        }
+
+         vector<long long>suffix(n);
+
+         suffix[n - 1] = 1;//if there is no element on right
+         for(int i = n - 2;i >= 0;i--)
+         {
+            if(nums[i + 1] != 0 && suffix[i + 1] > LLONG_MAX / nums[i + 1])
+            {
+                suffix[i] = LLONG_MAX;
+            }
+            else
+            {
+             suffix[i] = suffix[i + 1] * (long long)nums[i + 1];
+            }
+         }
+
+         for(int i = 0;i < n;i++)
+         {
+            if(leftSum[i] == suffix[i]) return i;
+         }
+
+         return -1;
+    }
+};
