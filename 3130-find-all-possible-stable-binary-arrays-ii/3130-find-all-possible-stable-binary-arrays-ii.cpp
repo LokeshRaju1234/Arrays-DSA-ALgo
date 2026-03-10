c7172e49705148113882e629af447b7d915c245a
LokeshRaju <loki65628@gmail.com>
2026-03-11 00:09:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3130-find-all-possible-stable-binary-arrays-ii/3130-find-all-possible-stable-binary-arrays-ii.cpp
+++ 3130-find-all-possible-stable-binary-arrays-ii/3130-find-all-possible-stable-binary-arrays-ii.cpp
@@ -1,43 +1,21 @@
 class Solution {
 public:
-    constexpr static int MOD = 1000000007;
     int numberOfStableArrays(int zero, int one, int limit) {
-        vector<vector<vector<int>>> dp(
-            zero + 1, vector<vector<int>>(one + 1, vector<int>(2)));
-        for (int i = 0; i <= zero; i++) {
-            for (int j = 0; j <= one; j++) {
-                for (int lastBit = 0; lastBit <= 1; lastBit++) {
-                    if (i == 0) {
-                        if (lastBit == 0 || j > limit) {
-                            dp[i][j][lastBit] = 0;
-                        } else {
-                            dp[i][j][lastBit] = 1;
-                        }
-                    } else if (j == 0) {
-                        if (lastBit == 1 || i > limit) {
-                            dp[i][j][lastBit] = 0;
-                        } else {
-                            dp[i][j][lastBit] = 1;
-                        }
-                    } else if (lastBit == 0) {
-                        dp[i][j][lastBit] = dp[i - 1][j][0] + dp[i - 1][j][1];
-                        if (i > limit) {
-                            dp[i][j][lastBit] -= dp[i - limit - 1][j][1];
-                        }
-                    } else {
-                        dp[i][j][lastBit] = dp[i][j - 1][0] + dp[i][j - 1][1];
-                        if (j > limit) {
-                            dp[i][j][lastBit] -= dp[i][j - limit - 1][0];
-                        }
-                    }
-                    dp[i][j][lastBit] %= MOD;
-                    if (dp[i][j][lastBit] < 0) {
-                        dp[i][j][lastBit] += MOD;
-                    }
-                }
+        const int MOD = 1e9 + 7;
+        vector<vector<array<long,2>>> dp(
+            zero+1, vector<array<long,2>>(one+1, {0LL,0LL}));
+
+        for (int i = 1; i <= min(zero,limit); i++) dp[i][0][0] = 1;
+        for (int j = 1; j <= min(one, limit); j++) dp[0][j][1] = 1;
+
+        for (int i = 1; i <= zero; i++) {
+            for (int j = 1; j <= one; j++) {
+                long over0 = (i-limit >= 1) ? dp[i-limit-1][j][1] : 0;
+                long over1 = (j-limit >= 1) ? dp[i][j-limit-1][0] : 0;
+                dp[i][j][0] = (dp[i-1][j][0] + dp[i-1][j][1] - over0 + MOD) % MOD;
+                dp[i][j][1] = (dp[i][j-1][0] + dp[i][j-1][1] - over1 + MOD) % MOD;
             }
         }
-
         return (dp[zero][one][0] + dp[zero][one][1]) % MOD;
     }
 };
