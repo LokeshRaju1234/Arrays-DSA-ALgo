c25cf3c29b1ebdcf3cb90cdf15455d392e16d28f
LokeshRaju <loki65628@gmail.com>
2026-03-12 08:48:50 +0530

    Time: 483 ms (12.56%), Space: 51.1 MB (22.95%) - LeetHub

--- 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.cpp
+++ 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.cpp
@@ -1,33 +1,42 @@
 class Solution {
 public:
-    int numberOfStableArrays(int zero, int one, int limit) {
-        vector<vector<vector<long long>>> dp(
-            zero + 1, vector<vector<long long>>(one + 1, vector<long long>(2)));
-        long long mod = 1e9 + 7;
-        for (int i = 0; i <= min(zero, limit); i++) {
-            dp[i][0][0] = 1;
+vector<vector<vector<int>>>dp;
+int MOD = 1e9 + 7;
+    int numberOfStableArrays(int zero, int one, int limit) 
+    {
+        dp.assign(zero + 1,vector<vector<int>>(one + 1, vector<int>(2,-1)));
+        int startWithZero = solve(zero,one,1,limit);
+        int startWithOne = solve(zero,one,0,limit);
+        return (startWithZero + startWithOne) % MOD;
+    }
+
+    int solve(int zero,int one,int lastWasOne,int limit)
+    {
+        if(zero == 0 && one == 0)
+        {
+            return 1;
         }
-        for (int j = 0; j <= min(one, limit); j++) {
-            dp[0][j][1] = 1;
+
+        if(dp[zero][one][lastWasOne] != -1)
+        {
+            return dp[zero][one][lastWasOne];
         }
-        for (int i = 1; i <= zero; i++) {
-            for (int j = 1; j <= one; j++) {
-                if (i > limit) {
-                    dp[i][j][0] = dp[i - 1][j][0] + dp[i - 1][j][1] -
-                                  dp[i - limit - 1][j][1];
-                } else {
-                    dp[i][j][0] = dp[i - 1][j][0] + dp[i - 1][j][1];
-                }
-                dp[i][j][0] = (dp[i][j][0] % mod + mod) % mod;
-                if (j > limit) {
-                    dp[i][j][1] = dp[i][j - 1][1] + dp[i][j - 1][0] -
-                                  dp[i][j - limit - 1][0];
-                } else {
-                    dp[i][j][1] = dp[i][j - 1][1] + dp[i][j - 1][0];
-                }
-                dp[i][j][1] = (dp[i][j][1] % mod + mod) % mod;
+        long long res = 0;
+        if(lastWasOne)//explore 0's
+        {
+            for(int len = 1;len <= min(zero,limit);len++)
+            {
+                res = (res + solve(zero - len,one,0,limit)) % MOD;
             }
         }
-        return (dp[zero][one][0] + dp[zero][one][1]) % mod;
+        else
+        {
+            for(int len = 1;len <= min(one,limit);len++)
+            {
+                res = (res + solve(zero,one - len,1,limit)) % MOD;
+            }
+        }
+
+        return dp[zero][one][lastWasOne] = res;
     }
 };
