0b9aaaf778f2e46217eae09ca1b384d5c04e1a12
LokeshRaju <loki65628@gmail.com>
2026-03-19 17:52:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.cpp
+++ 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.cpp
@@ -1,42 +1,36 @@
+
 class Solution {
 public:
-vector<vector<vector<int>>>dp;
 int MOD = 1e9 + 7;
-    int numberOfStableArrays(int zero, int one, int limit) 
-    {
-        dp.assign(zero + 1,vector<vector<int>>(one + 1, vector<int>(2,-1)));
-        int startWithZero = solve(zero,one,1,limit);
-        int startWithOne = solve(zero,one,0,limit);
-        return (startWithZero + startWithOne) % MOD;
+    int numberOfStableArrays(int zero, int one, int limit) {
+        int startwith0 = solve(zero,one,false,limit);
+        int startwith1 = solve(zero,one,true,limit);
+        return (startwith0 + startwith1) % MOD;
     }
 
-    int solve(int zero,int one,int lastWasOne,int limit)
+    int solve(int zero,int one,bool lastWasone,int limit)
     {
         if(zero == 0 && one == 0)
         {
             return 1;
         }
 
-        if(dp[zero][one][lastWasOne] != -1)
-        {
-            return dp[zero][one][lastWasOne];
-        }
-        long long res = 0;
-        if(lastWasOne)//explore 0's
+        int res = 0;
+        if(lastWasone)//explore 0's
         {
             for(int len = 1;len <= min(zero,limit);len++)
             {
-                res = (res + solve(zero - len,one,0,limit)) % MOD;
+            res = (res + solve(zero - len,one,false,limit)) % MOD;
             }
         }
-        else
+        else //explore 1's
         {
             for(int len = 1;len <= min(one,limit);len++)
             {
-                res = (res + solve(zero,one - len,1,limit)) % MOD;
+            res = (res + solve(zero,one - len,true,limit)) % MOD;
             }
         }
 
-        return dp[zero][one][lastWasOne] = res;
+        return res;
     }
 };
