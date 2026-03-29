0f373100f3fbff8f95b16cd3a4f964cdcfba246e
LokeshRaju <loki65628@gmail.com>
2026-03-29 23:46:03 +0530

    Time: 0 ms (100%), Space: 8.2 MB (67.1%) - LeetHub

---
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) 
+    {
+        //only we can swap is wiht the indices with even pos and odd pos 
+        //s1 = "abcd", s2 = "cdab" -->only we can swap indices (0,2) and (1,3) which gives j - i = 2
+
+        bool evenpos = (s1[0] == s2[0] && s1[2] == s2[2]) ||
+        (s1[0] == s2[2] && s1[2] == s2[0]);
+
+        bool oddpos = (s1[1] == s2[1] && s1[3] == s2[3]) ||
+        (s1[1] == s2[3] && s1[3] == s2[1]);
+
+        return evenpos && oddpos;
+    }
+};
