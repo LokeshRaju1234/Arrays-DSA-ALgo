0ac03e74f21ea510f797fb5385a05014481c5b73
LokeshRaju <loki65628@gmail.com>
2026-04-03 07:29:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.cpp
+++ 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     int maximumAmount(vector<vector<int>>& coins) {
-        int m = coins.size(), n = coins[0].size();
-        vector memo(m, vector(n, array<int, 3>{INT_MIN, INT_MIN, INT_MIN}));
-
-        function<int(int, int, int)> dfs = [&](int i, int j, int k) -> int {
-            if (i >= m || j >= n) {
-                return INT_MIN;
+        const int m = coins.size(), n = coins[0].size();
+        vector<vector<long long>> dp(m, vector<long long>(n));
+        vector<vector<long long>> temp(m, vector<long long>(n));
+        temp[m - 1][n - 1] = coins[m - 1][n - 1];
+        for(int i = m - 1; i >= 0; --i)
+            for(int j = n - 1; j >= 0; --j){
+                if (i == m - 1 && j == n - 1) continue;
+                long long val = LLONG_MIN;
+                if (i < m - 1) val = max(val, coins[i][j] + temp[i+1][j]);
+                if (j < n - 1) val = max(val, coins[i][j] + temp[i][j+1]);
+                temp[i][j] = val;
             }
-
-            int x = coins[i][j];
-            // arrive at the destination
-            if (i == m - 1 && j == n - 1) {
-                return k > 0 ? max(0, x) : x;
-            }
-
-            int res = memo[i][j][k];
-            if (res != INT_MIN) {
-                return res;
-            }
-            // not neutralize
-            res = max(dfs(i + 1, j, k), dfs(i, j + 1, k)) + x;
-            if (k > 0 && x < 0) {
-                // neutralize
-                res = max({res, dfs(i + 1, j, k - 1), dfs(i, j + 1, k - 1)});
-            }
-            return memo[i][j][k] = res;
-        };
-
-        return dfs(0, 0, 2);
+        for(int k = 0; k < 2; ++k){
+            dp[m-1][n-1] = (coins[m-1][n-1] < 0? 0 : coins[m-1][n-1]);
+            for(int i = m - 1; i >= 0; --i)
+                for(int j = n - 1; j >= 0; --j){
+                    if (i == m - 1 && j == n - 1) continue;
+                    long long val = LLONG_MIN;
+                    int aCur = coins[i][j];
+                    int cur = (aCur < 0? 0 : coins[i][j]);
+                    if (i < m - 1) val = max(val, cur + temp[i+1][j]);
+                    if (j < n - 1) val = max(val, cur + temp[i][j+1]);
+                    if (i < m - 1) val = max(val, aCur + dp[i+1][j]);
+                    if (j < n - 1) val = max(val, aCur + dp[i][j+1]);
+                    dp[i][j] = val;
+                }
+            temp = dp;
+        } return dp[0][0];
     }
 };
